141bcfe6c9bd7351303c32caa8cbe178fc70f96d
Raz <raz.chiriac@gmail.com>
2017-02-06 14:59:56 -0500

    Wave Spawner

---
+++ Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour
+{
+
+	public Transform enemyPrefab;
+	public Transform spawnPoint;
+	public float timeBetweenWaves = 5f;
+	private float countdown = 2f;
+	private int waveIndex = 0;
+
+	void Update ()
+	{
+		if (countdown <= 0) {
+			StartCoroutine(SpawnWave ());
+			countdown = timeBetweenWaves;
+		}
+
+		countdown -= Time.deltaTime;
+	}
+
+	IEnumerator SpawnWave ()
+	{
+		waveIndex++;
+		for (int i = 0; i < waveIndex; i++) {
+			SpawnEnemy ();
+			yield return new WaitForSeconds (0.5f);
+		}
+	}
+
+	void SpawnEnemy ()
+	{
+		Instantiate (enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+	}
+
+}
